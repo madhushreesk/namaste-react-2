44951303c33cb5dafb8b6abf8ec4c1bcc27cfd94
madhushreesk <88674169+madhushreesk@users.noreply.github.com>
2023-11-23 19:20:53 +0530

    created res cards

--- App.js
+++ App.js
@@ -9,6 +9,8 @@ import twiggitoLogo from "./assets/twiggitoLogo.png";
 // - search
 // - RestaurantContainer
 // -- resCard
+//    --Img
+//    --Name of res, star rating, cuisine
 // Footer component
 // - copyright, address, links
 
@@ -31,10 +33,50 @@ const Header = () => {
   );
 };
 
+const RestaurantCard = ({ resName, cuisine, star, eta }) => {
+  console.log(resName);
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        src="https://images.theconversation.com/files/368263/original/file-20201109-22-lqiq5c.jpg?ixlib=rb-1.1.0&rect=10%2C0%2C6699%2C4476&q=45&auto=format&w=926&fit=clip"
+        alt="res-logo"
+      />
+      <h3>{resName}</h3>
+      <h4>{cuisine}</h4>
+      <h4>{star}</h4>
+      <h4>{eta}</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard
+          resName={"Meghanas food"}
+          cuisine={"Biriyani, North Indian"}
+          eta={"38mins"}
+          star={"4.4"}
+        />
+        <RestaurantCard
+          resName={"KFC"}
+          cuisine={"Burger, Biriyani"}
+          eta={"48mins"}
+          star={"4.5"}
+        />
+      </div>
+    </div>
+  );
+};
+
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };
