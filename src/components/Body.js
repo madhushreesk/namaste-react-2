8742fb856fa8a99f7d3878175e544cee246d85c3
madhushreesk <88674169+madhushreesk@users.noreply.github.com>
2023-12-07 17:14:53 +0530

    Made use of swiggy live API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,6 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
   const [listOfRestaurant, setListOfRestaurant] = useState(resList);
@@ -10,6 +10,20 @@ const Body = () => {
       (res) => res.avgRating > 4
     );
     setListOfRestaurant(topRatedRestaurants);
+  };
+
+  useEffect(() => {
+    fetchData();
+    console.log("useEffect called");
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
   };
 
   return (
