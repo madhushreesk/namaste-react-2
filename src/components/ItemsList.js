ab5b77d5e5fe981041afa15c8daad83dd61d46dc
madhushreesk <88674169+madhushreesk@users.noreply.github.com>
2023-12-19 19:19:24 +0530

    configured RTL and babel

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -8,7 +8,6 @@ const ItemsList = ({ itemCards }) => {
   const dispatch = useDispatch();
 
   const handleAddItem = (item) => {
-    console.log(item);
     dispatch(addItem(item));
     setAddItemClick(true);
   };
@@ -20,51 +19,42 @@ const ItemsList = ({ itemCards }) => {
   return (
     <div>
       {itemCards?.map((item) => (
-        <ul>
-          <div
-            key={item.card.info.id}
-            className="p-2 m-2 border-gray-200 text-left flex justify-between"
-          >
-            <div className="flex justify-between py-2">
-              <div className="w-10/12 flex flex-col">
-                <span className="font-bold">{item?.card?.info?.name}</span>
-                <span className="font-bold">
-                  {" Rs. "}
-                  {item?.card?.info?.price / 100 ||
-                    item?.card?.info?.defaultPrice / 100}
-                </span>
-
-                <div className="text-xs w-9/12">
-                  {item?.card?.info?.description}
-                </div>
-                <div className="my-1">
-                  <button
-                    onClick={() => handleAddItem(item)}
-                    className="bg-slate-300 p-2 shadow-lg  hover:bg-slate-200"
-                  >
-                    Add +
-                  </button>
-                  {addItemClick && (
-                    <button
-                      onClick={() => handleRemoveItem(item)}
-                      className="bg-slate-300 p-2 ml-2 shadow-lg  hover:bg-slate-200"
-                    >
-                      Remove -
-                    </button>
-                  )}
-                </div>
-              </div>
-
-              <img
-                className="w-2/12 h-28 rounded-sm"
-                src={CDN_IMAGE_URL + item?.card?.info?.imageId}
-                alt="imageRes"
-              />
+        <div
+          key={item.card.info.id}
+          className="p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between"
+        >
+          <div className="w-9/12">
+            <div className="py-2">
+              <span className="font-bold">{item?.card?.info?.name}</span>
+              <span className="font-bold">
+                {" Rs. "}
+                {item?.card?.info?.price / 100 ||
+                  item?.card?.info?.defaultPrice / 100}
+              </span>
             </div>
+            <p className="text-xs">{item?.card?.info?.description}</p>
+            <button
+              onClick={() => handleAddItem(item)}
+              className="bg-slate-300 p-2  mt-2 shadow-lg  hover:bg-slate-200"
+            >
+              Add +
+            </button>
+            {/* {addItemClick && (
+              <button
+                onClick={() => handleRemoveItem(item)}
+                className="bg-slate-300 p-2 mt-2 ml-3 shadow-lg  hover:bg-slate-200"
+              >
+                Remove -
+              </button>
+            )} */}
           </div>
 
-          <div className="w-full h-[1.5px] bg-gray-200 my-4"></div>
-        </ul>
+          <img
+            className="w-2/12 h-28 rounded-sm"
+            src={CDN_IMAGE_URL + item?.card?.info?.imageId}
+            alt="imageRes"
+          />
+        </div>
       ))}
     </div>
   );
